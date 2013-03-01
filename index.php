279a4ecf4e936686ccbb4ea76235b2773c4a56cf
Seth <seth@collectiveaccess.org>
2013-02-28 23:13:40 -0500

    Sync

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * ----------------------------------------------------------------------
  *
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2008-2013 Whirl-i-Gig
+ * Copyright 2008-2012 Whirl-i-Gig
  *
  * For more information visit http://www.CollectiveAccess.org
  *
@@ -26,13 +26,13 @@
  * ----------------------------------------------------------------------
  */
 	define("__CA_MICROTIME_START_OF_REQUEST__", microtime());
-	define("__CA_BASE_MEMORY_USAGE__", memory_get_usage(true));
-	define("__CA_APP_TYPE__", "PROVIDENCE");
+	define("__CA_SEARCH_IS_FOR_PUBLIC_DISPLAY__", 1);
+	define("__CA_APP_TYPE__", "PAWTUCKET");
 	
 	if (!file_exists('./setup.php')) { print "No setup.php file found!"; exit; }
 	require('./setup.php');
+	define("__CA_BASE_MEMORY_USAGE__", memory_get_usage(true));
 	
-	caWriteServerConfigHints();
 	// connect to database
 	$o_db = new Db(null, null, false);
 	if (!$o_db->connected()) {
@@ -40,75 +40,86 @@
 		require_once(__CA_BASE_DIR__."/themes/default/views/system/configuration_error_html.php");
 		exit();
 	}
-	$g_monitor = new ApplicationMonitor();
-	if ($g_monitor->isEnabled()) { $o_db->setMonitor($g_monitor); }
-	
 	//
 	// do a sanity check on application and server configuration before servicing a request
 	//
-	require_once(__CA_APP_DIR__.'/lib/ca/ConfigurationCheck.php');
+	
+	require_once(__CA_APP_DIR__.'/lib/pawtucket/ConfigurationCheck.php');
 	ConfigurationCheck::performQuick();
 	if(ConfigurationCheck::foundErrors()){
-		if (defined('__CA_ALLOW_AUTOMATIC_UPDATE_OF_DATABASE__') && __CA_ALLOW_AUTOMATIC_UPDATE_OF_DATABASE__ && $_REQUEST['updateSchema']) { 
-			ConfigurationCheck::updateDatabaseSchema();
-		} else {
-			ConfigurationCheck::renderErrorsAsHTMLOutput();
-		}
+		ConfigurationCheck::renderErrorsAsHTMLOutput();
 		exit();
 	}
 	
 	$app = AppController::getInstance();
 	
-	$g_request = $req = $app->getRequest();
-	$g_response = $resp = $app->getResponse();
+	$g_request = $app->getRequest();
+	$resp = $app->getResponse();
+
+	// TODO: move this into a library so $_, $g_ui_locale_id and $g_ui_locale gets set up automatically
+	$va_ui_locales = $g_request->config->getList('ui_locales');
+	if ($vs_lang = $g_request->getParameter('lang', pString)) {
+		if (in_array($vs_lang, $va_ui_locales)) {
+			$g_request->session->setVar('lang', $vs_lang);
+		}
+	}
+	if (!($g_ui_locale = $g_request->session->getVar('lang'))) {
+		$g_ui_locale = $va_ui_locales[0];
+	}
+	
+	if (!in_array($g_ui_locale, $va_ui_locales)) {
+		$g_ui_locale = $va_ui_locales[0];
+	}
+	$t_locale = new ca_locales();
+	$g_ui_locale_id = $t_locale->localeCodeToID($g_ui_locale);		// get current UI locale as locale_id	  (available as global)
+	$_ = array();
+	if (file_exists($vs_theme_specific_locale_path = $g_request->getThemeDirectoryPath().'/locale/'.$g_ui_locale.'/messages.mo')) {
+		$_[] = new Zend_Translate('gettext', $vs_theme_specific_locale_path, $g_ui_locale);
+	}
+	$_[] = new Zend_Translate('gettext', __CA_APP_DIR__.'/locale/'.$g_ui_locale.'/messages.mo', $g_ui_locale);
+
+	$g_request->reloadAppConfig();	// need to reload app config to reflect current locale
+	
+	//
+	// PageFormat plug-in generates header/footer shell around page content
+	//
+	if (!$g_request->isAjax() && !$g_request->isDownload()) {
+		require_once(__CA_LIB_DIR__.'/ca/PageFormat.php');
+		$app->registerPlugin(new PageFormat());
+	} else {
+		require_once(__CA_LIB_DIR__.'/ca/AjaxFooter.php');
+		$app->registerPlugin(new AjaxFooter());
+	}
+	
+	//
+	// ContentCaching plug-in caches output of selected pages for performance
+	//
+	require_once(__CA_LIB_DIR__.'/ca/ContentCaching.php');
+	$app->registerPlugin(new ContentCaching());
+	
+	//
+	// Load mobile
+	//
+	if (caDeviceIsMobile()) { JavascriptLoadManager::register('mobile'); }
 	
 	// Prevent caching
 	$resp->addHeader("Cache-Control", "no-cache, must-revalidate");
 	$resp->addHeader("Expires", "Mon, 26 Jul 1997 05:00:00 GMT");
 	
 	//
-	// Don't try to authenticate when doing a login attempt (will be done in the action, of course)
-	//
-	if (!preg_match("/^\/system\/auth\/(dologin|login)/i", $req->getPathInfo())) {
-		$vb_auth_success = $req->doAuthentication(array('noPublicUsers' => true));
-
-		if(!$vb_auth_success) {
-			$resp->sendResponse();
-			$req->close();
-			exit;
-		}
-	}
-
-	// TODO: move this into a library so $_, $g_ui_locale_id and $g_ui_locale gets set up automatically
-	$g_ui_locale_id = $req->user->getPreferredUILocaleID();			// get current UI locale as locale_id	 			(available as global)
-	$g_ui_locale = $req->user->getPreferredUILocale();				// get current UI locale as locale string 			(available as global)
-	$g_ui_units_pref = $req->user->getPreference('units');			// user's selected display units for measurements 	(available as global)
-	
-	if((!isset($_locale)) || ($g_ui_locale != $_COOKIE['CA_'.__CA_APP_NAME__.'_ui_locale'])) {
-		if(!initializeLocale($g_ui_locale)) die("Error loading locale ".$g_ui_locale);
-		$req->reloadAppConfig();
-	}
-
-	global $ca_translation_cache;
-	$ca_translation_cache = array();
-	
-	//
-	// PageFormat plug-in generates header/footer shell around page content
-	//
-	require_once(__CA_APP_DIR__.'/lib/ca/PageFormat.php');
-	if (!$req->isAjax() && !$req->isDownload()) {
-		$app->registerPlugin(new PageFormat());
-	}
-	
-	//
 	// Dispatch the request
 	//
+	$vb_auth_success = $g_request->doAuthentication(array('dont_redirect' => true, 'noPublicUsers' => false));
 	$app->dispatch(true);
-	
+
 	//
 	// Send output to client
 	//
 	$resp->sendResponse();
-	$req->close();
 	
+	// Note url of this page as "last page"
+	if (($g_request->getController() != 'LoginReg') && (!$g_request->isAjax()) && (!$g_request->getParameter('dont_set_pawtucket2_last_page', pInteger))) {	// the 'dont_set_pawtucket2_last_page' is a lame-but-effective way of suppressing recording of something we don't want to be a "last page" (and potentially redirected to)
+		$g_request->session->setVar('pawtucket2_last_page', $g_request->getFullUrlPath());
+	}
+	$g_request->close();
 ?>
