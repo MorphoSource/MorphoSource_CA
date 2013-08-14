aae41f4ca6c772ca4e8c8145cdf3aa9ef9f59e8f
maria <maria@tumbler.(none)>
2013-08-14 11:56:56 -0400

    version updated

--- support/sql/migrations/17.sql
+++ support/sql/migrations/17.sql
@@ -3,3 +3,8 @@ ADD COLUMN scanner_calibration_shading_correction TINYINT UNSIGNED NOT NULL,
 ADD COLUMN scanner_calibration_flux_normalization TINYINT UNSIGNED NOT NULL,
 ADD COLUMN scanner_calibration_geometric_calibration TINYINT UNSIGNED NOT NULL,
 DROP COLUMN scanner_calibration_check;
+
+
+/* -------------------------------------------------------------------------------- */
+/* Always add the update to ca_schema_updates at the end of the file */
+INSERT IGNORE INTO ca_schema_updates (version_num, datetime) VALUES (17, unix_timestamp());
