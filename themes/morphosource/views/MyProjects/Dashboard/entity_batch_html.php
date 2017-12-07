b4d975b2eae0cb803c5370ca10303e233483ae06
Julie Winchester <julie.winchester@duke.edu>
2017-12-07 13:52:57 -0500

    Fixed line length and removed unnecessary commented code, added new comments

--- themes/morphosource/views/MyProjects/Dashboard/entity_batch_html.php
+++ themes/morphosource/views/MyProjects/Dashboard/entity_batch_html.php
@@ -2,9 +2,10 @@
 	$vs_message = $this->getVar("message");
 	$t_media = new ms_media();
 
+	// Entity viewer batch editing menu
 	print "<div id='entityMediaBatchFormContainer'>";
 
-	// media batch edit general form
+	// General options tab menu
 	print "<div id='mediaBatchGeneralContainer' style=''>";
 
 	if ($vs_message['general']) {
@@ -15,121 +16,81 @@
 		null, 'post', 'multipart/form-data', '', 
 		array('disableUnsavedChangesWarning' => true));
 
-
 	print "<div id='batchColLeft'>";
 
-// media bibliographic citation
+	// Media bibliographic citation
 	print "<div id='mediaBibLookupContainer' ".
 		"style='margin: 15px 0px 0px 0px;'>";
 	print "<h2>Media bibliography</h2>";
-	print "<div class='formLabel entityBatchFormItem'>Look up a bibliographic citation:</br>";
+	print "<div class='formLabel entityBatchFormItem'>Look up a bibliographic 
+		citation:</br>";
 	print caHTMLTextInput('bibliography_lookup', 
 		array('id' => 'msBibliographyID', 'class' => 'lookupBg'), 
 		array('width' => '350px', 'height' => 1));
 	print "<input type='hidden' value='' name='bibliography_id' ".
 		"id='bibliography_id'>";
 	print "</div>";
-
-	// if($vs_message['bibliography']){
-	// 	print "<div class='formErrors'>".$vs_message['bibliography']."</div>";
-	// }
-
 	print "</div><!-- end mediaBibLookupContainer -->";
 
-	// media citation instructions
+	// Media citation instructions
 	print "<div id='mediaCitationContainer' style='margin: 15px 0px 0px 0px;'>";
 	print "<h2>Media citation instructions</h2>";
-	// print "<div class='formLabel'>Media citation instructions</div>";
-
 	print "<div class='formLabel entityBatchFormItem' style='width: 390px;'>".
 		$t_media->htmlFormElement("media_citation_instruction1", "^ELEMENT").
 		"provided access to these data".
 		$t_media->htmlFormElement("media_citation_instruction2", "^ELEMENT").
 		$t_media->htmlFormElement("media_citation_instruction3", "^ELEMENT").
-		". The files were downloaded from www.MorphoSource.org, Duke University.</div>";
-
-	// if ($vs_message['media_citation']) {
-	// 	print "<div class='formErrors'>".$vs_message['media_citation']."</div>";
-	// }
-
+		". The files were downloaded from www.MorphoSource.org, 
+		Duke University.</div>";
 	print "</div><!-- end mediaCitationContainer -->";
 
-	// grant support
+	// Grant support
 	print "<div id='mediaGrantSupportContainer' ".
 		"style='margin: 15px 0px 0px 0px;'>";
 	print "<h2>Support information:</h2>";
-	
 	print "<div class='formLabel entityBatchFormItem'>Grant support:</br>";
 	print $t_media->htmlFormElement('grant_support', '^ELEMENT', 
 		array('width' => '350px'));
 	print "</div>";
-
-	// if($vs_message['grant_support']){
-	// 	print "<div class='formErrors'>".$vs_message['grant_support']."</div>";
-	// }
-
 	print "</div><!-- end mediaGrantSupportContainer -->";
 
 	print "</div><!-- end batchColLeft -->";
 
 	print "<div id='batchColRight'>";
 
-	// copyright information
+	// Copyright information
 	print "<div id='mediaCopyrightContainer' style='margin: 15px 0px 0px 0px;'>";
 	print "<h2>Copyright information</h2>";
-
 	print "<div class='formLabel entityBatchFormItem'>Permission:</br>";
 	print $t_media->htmlFormElement('copyright_permission', '^ELEMENT', 
 		array('width' => '350px'))."</div>";
-
 	print "<div class='formLabel entityBatchFormItem'>License:</br>";
 	print $t_media->htmlFormElement('copyright_license', '^ELEMENT', 
 		array('width' => '350px'))."</div>";
-
 	print "<div class='formLabel entityBatchFormItem'>Copyright Holder:</br>";
 	print $t_media->htmlFormElement('copyright_info', 
 		'^ELEMENT', array('width' => '350px'))."</div>";
-
-	// if($vs_message['copyright']){
-	// 	print "<div class='formErrors'>".$vs_message['copyright']."</div>";
-	// }
-
 	print "</div><!-- end mediaCopyrightContainer -->";
 
-	print "<div id='batchPublicationLinkContainer' style='margin: 15px 0px 0px 0px;'>";
+	// Link to edit publication status 
+	print "<div id='batchPublicationLinkContainer' 
+		style='margin: 15px 0px 0px 0px;'>";
 	print "<h2>Publication information</h2>";
-	print "<div style='margin-left: 5px;'>".caNavLink($this->request, _t("Click here to batch edit publication settings."), "", "MyProjects", "Media", "reviewPublicationSettings")."</div>";
+	print "<div style='margin-left: 5px;'>".
+		caNavLink($this->request, 
+			_t("Click here to batch edit publication settings."), 
+			"", "MyProjects", "Media", "reviewPublicationSettings").
+		"</div>";
 	print "</div><!-- end batchPublicationLinkContainer -->";
 
 	print "</div><!-- end batchColRight -->";
 
-	
-
-
 	print "</form><!-- end batchGeneralForm -->";
 	print "</div><!-- end mediaBatchGeneralContainer -->";
 
-	// media batch edit scan origin form
+	// Scan origin options tab menu
 	print "<div id='mediaBatchScanOriginContainer' style='display: none;'>";
 		
-	// $va_scan_fields = array('facility_id', 'scanner_id', 'scanner_x_resolution', 
-	// 	'scanner_y_resolution', 'scanner_z_resolution', 'scanner_voltage', 
-	// 	'scanner_amperage', 'scanner_watts', 'scanner_exposure_time', 
-	// 	'scanner_filter', 'scanner_projections', 'scanner_frame_averaging', 
-	// 	'scanner_wedge', 'scanner_calibration', 
-	// 	'scanner_calibration_description', 'scanner_technicians');
-	$scanner_calibration_fields = array(
-		'scanner_calibration_shading_correction', 
-		'scanner_calibration_flux_normalization', 
-		'scanner_calibration_geometric_calibration');
-	// $va_float_fields = array('facility_id', 'scanner_id', 
-	// 	'scanner_x_resolution', 'scanner_y_resolution', 'scanner_z_resolution', 
-	// 	'scanner_voltage', 'scanner_amperage', 'scanner_watts', 
-	// 	'scanner_exposure_time', 'scanner_filter', 'scanner_projections', 
-	// 	'scanner_frame_averaging', 'scanner_wedge', 'scanner_calibration', 
-	// 	'scanner_calibration_description', 'scanner_technicians');
-	
-
 	print caFormTag($this->request, 'batchScanOriginSave', 
 		'batchScanOriginForm', null, 'post', 'multipart/form-data', '', 
 		array('disableUnsavedChangesWarning' => true));
@@ -137,36 +98,42 @@
 	print "<div id='batchColLeft'>";
 
 	// Facility and scanner options
-	print "<div id='mediaFacilityScannerContainer' style='margin: 15px 0px 0px 0px;'>";
+	print "<div id='mediaFacilityScannerContainer' 
+		style='margin: 15px 0px 0px 0px;'>";
 	print "<h2>Facility and scanner</h2>";
-
-	print "<div class='formLabel entityBatchFormItem'>Find the facilty this media file was created at:</br>";
-	print $t_media->htmlFormElement('facility_id', '^ELEMENT', array('id' => 'msFacilityID', 'nullOption' => '-', "width" => '350px'))."</div>";
-
+	print "<div class='formLabel entityBatchFormItem'>Find the facilty this 
+		media file was created at:</br>";
+	print $t_media->htmlFormElement('facility_id', '^ELEMENT', 
+		array('id' => 'msFacilityID', 'nullOption' => '-', "width" => '350px')).
+		"</div>";
 	print "<div>";
 	print "<div class='formLabel entityBatchFormItem'>Choose scanner used:</br>";
-	print $t_media->htmlFormElement('scanner_id', '^ELEMENT', array('id' => 'msScannerID', 'nullOption' => '-', "width" => '350px'))."</div>";
+	print $t_media->htmlFormElement('scanner_id', '^ELEMENT', 
+		array('id' => 'msScannerID', 'nullOption' => '-', "width" => '350px')).
+		"</div>";
 	print "</div>";
-
 	print "</div><!-- end mediaFacilityScannerContainer -->";
 
 	// Scanner Options
-	print "<div id='mediaScannerOptionsContainer' style='margin: 15px 0px 0px 0px;'>";
-	print "<h2>Scanner options</h2>";
-
 	$va_scan_option_fields = array('scanner_x_resolution', 
 		'scanner_y_resolution', 'scanner_z_resolution', 'scanner_voltage', 
 		'scanner_amperage', 'scanner_watts', 'scanner_exposure_time', 
 		 'scanner_filter', 'scanner_projections', 'scanner_frame_averaging',  
 		'scanner_wedge');
-	$va_clear_fields = array('scanner_z_resolution', 'scanner_watts', 'scanner_filter');
+	$va_clear_fields = array('scanner_z_resolution', 'scanner_watts', 
+		'scanner_filter');
 	$std_size_fields = array('scanner_x_resolution', 
 		'scanner_y_resolution', 'scanner_z_resolution', 'scanner_voltage', 
 		'scanner_amperage', 'scanner_watts', 'scanner_exposure_time');
 
+	print "<div id='mediaScannerOptionsContainer' 
+		style='margin: 15px 0px 0px 0px;'>";
+	print "<h2>Scanner options</h2>";
 	foreach ($va_scan_option_fields as $f) {
 		print "<div id='scanOption".str_replace('_', '', ucwords($f, '_')).
-			"' style='margin: 0px 0px 0px 0px;".((in_array($f, $std_size_fields)) ? " width: 120px;": "")." float: left;'>";
+			"' style='margin: 0px 0px 0px 0px;".
+			((in_array($f, $std_size_fields)) ? " width: 120px;": "").
+			" float: left;'>";
 		$vs_suffix = $t_media->getFieldInfo($f, 'SUFFIX');
 		print $t_media->htmlFormElement($f,
 			"<div class='formLabel'>^LABEL<br>^ELEMENT {$vs_suffix}".
@@ -177,18 +144,21 @@
 			print "<div style='clear: both;'><!-- empty --></div>";
 		}
 	}
-
 	print "</div><!-- end mediaScannerOptionsContainer -->";
 
 	print "</div><!-- end batchColLeft -->";
 
-
 	print "<div id='batchColRight'>";
 
 	// Calibration options
-	print "<div id='mediaCalibrationOptionsContainer' style='margin: 15px 0px 0px 0px;'>";
-	print "<h2>Calibration options</h2>";
+	$scanner_calibration_fields = array(
+		'scanner_calibration_shading_correction', 
+		'scanner_calibration_flux_normalization', 
+		'scanner_calibration_geometric_calibration');
 
+	print "<div id='mediaCalibrationOptionsContainer' 
+		style='margin: 15px 0px 0px 0px;'>";
+	print "<h2>Calibration options</h2>";
 	print "<div style='margin: 0px 0px 15px 5px;'>";
 	foreach ($scanner_calibration_fields as $sc_f) {
 		print $t_media->htmlFormElement($sc_f,
@@ -198,10 +168,10 @@
 			"^ELEMENT {$vs_suffix}</div>");
 	}
 	print "</div>";
-
-	print "<div class='formLabel entityBatchFormItem'>Calibration description:</br>";
-	print $t_media->htmlFormElement('scanner_calibration_description', '^ELEMENT')."</div>";
-
+	print "<div class='formLabel entityBatchFormItem'>
+		Calibration description:</br>";
+	print $t_media->htmlFormElement('scanner_calibration_description', 
+		'^ELEMENT')."</div>";
 	print "</div><!-- end mediaCalibrationOptionsContainer -->";
 
 	// Other options
@@ -209,18 +179,16 @@
 	print "<h2>Other information</h2>";
 	print "<div class='formLabel entityBatchFormItem'>Technicians:</br>";
 	print $t_media->htmlFormElement('scanner_technicians', '^ELEMENT')."</div>";
+	print "</div>";
 	print "</div><!-- end mediaOthersContainer -->";
-	
+
 	print "</div><!-- end batchColRight -->";
 
-	print "</div>";
-
 	print "</form><!-- end batchScanOriginForm -->";
+
 	print "</div><!--  end entityMediaBatchFormContainer -->";
 
-	// print "</div>";
-
-	// save and cancel buttons
+	// Save and cancel buttons below form panes
 	print "<div id='batchButtonContainer' ".
 		"style='margin: 15px 0px 0px 0px; text-align: center;'>";
 	print "<a href='#' name='saveButton' class='button buttonLarge batchButton' ".
