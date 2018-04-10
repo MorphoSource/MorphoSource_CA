669dee03e528b4ea9b319fead37b612164a8c107
Julie Winchester <julia.m.winchester@gmail.com>
2018-04-10 16:56:32 -0400

    Added recordset db field [must be populated manually for existing db entries]

--- app/models/ms_specimens.php
+++ app/models/ms_specimens.php
@@ -819,6 +819,7 @@ class ms_specimens extends BaseModel {
 						$t_specimen->set('url', $va_specimen_info["data"]["dcterms:references"]);
 						$t_specimen->set('collector', $va_specimen_info["indexTerms"]["collector"]);
 						$t_specimen->set('collected_on', $va_specimen_info["indexTerms"]["datecollected"]);
+						$t_specimen->set('recordset', $va_specimen_info["indexTerms"]["recordset"]);
 						if($va_specimen_info["data"]["dwc:sex"]){
 							if(strpos(strtolower($va_specimen_info["data"]["dwc:sex"]), "female") !== false){
 								$t_specimen->set('sex', 'F');
