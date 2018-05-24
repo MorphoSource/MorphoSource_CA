2ea754caae9248590039e9e4fc619d72cc960646
Julia Winchester <jmw110@ea-boyerlab-morphosource-01.oit.duke.edu>
2018-05-24 14:02:58 -0400

    Fix to report page RSS link

--- themes/morphosource/views/About/report_html.php
+++ themes/morphosource/views/About/report_html.php
@@ -8,7 +8,7 @@
 		<p>MorphoSource provides summary reports of media, download usage, and download requests for media that represent specimens that have been reported to <a href='https://www.idigbio.org'>iDigBio</a>. The media report is formatted according to the Audubon Core metadata standard, and so can be incorporated into publisher reporting software, such as an IPT. All report files are linked and described in a single RSS feed, which can be used to receive regular report updates via automated download. Additionally, all report files are individually listed below, sorted by iDigBio publisher and recordset. For each report, there is a link to: <b>1)</b> a Comma Separated Values (CSV) spreadsheet with the primary media, download usage, or download request metadata; and <b>2)</b> an XML file encoded in Ecological Markup Language (EML) providing metadata about the CSV spreadsheet. These reports are updated as necessary on a daily basis.</p>
 	</div>
 	<div>
-		<b>RSS Feed: </b><a href='https://www.morphosource.org/rss/ms_rss.xml'>https://www.morphosource.org/rss/ms.rss</a>
+		<b>RSS Feed: </b><a href='https://www.morphosource.org/rss/ms_rss.xml'>https://www.morphosource.org/rss/ms_rss.xml</a>
 	</div>
 	<div class='dataReportDisclaimer'>
 		<p><b>IMPORTANT</b></p> 
