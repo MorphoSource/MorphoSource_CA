86a92c01551cb95313a76b73d3d682a96c6b05e0
Simon Choy <simon.choy@duke.edu>
2018-06-05 09:38:34 -0400

    general info 2 initial commit

---
+++ themes/morphosource/views/About/home.php
@@ -0,0 +1,40 @@
+<div class="blueRule"><!-- empty --></div>
+<h1>About MorphoSource</h1>
+<div class="textContent">
+	<div>
+        
+        
+        <div>MorphoSource is a digital repository where researchers, museum curators, and the general public can find, download, and upload 3D media representing physical objects, most commonly biological specimens. If you are a researcher, you can use MorphoSource to locate and download 3D media for analysis, and archive and share 3D media you have generated or are currently working on with collaborators, the community, and/or the public. If you are a museum curator, you can use MorphoSource to make digital media representing your collections available to scientific and lay communities. If you are a member of the general public, MorphoSource allows you to virtually explore the "raw data" of evolutionary biology and other sciences through digitally interacting with the biological specimens and other physical objects that underpin cutting-edge scientific thought.</div>
+
+
+        <h3>MorphoSource architecture, servers and data security</h3>
+        <div>MorphoSource runs on a virtual machine hosted by Duke University Research Computing. The server is backed up daily. Security patches and updates are applied every two weeks. MorphoSource data are replicated at two geographically separated storage facilities, on a third continuously updated tape archive and on a fourth set of tapes that are removed to off site storage every sixty days, as per Duke’s data preservation policy. In the next three years, MorphoSource will additionally replicate data to Penn State University servers.</div>
+
+        <h3>MorphoSource Funding</h3>
+        <div>Initial creation of MorphoSource was funded by Duke University Trinity College and Arts Sciences in 2012-13.  The developer Whirl-i-gig, Inc. created the initial application as a LAMP stack. At  that time Duke agreed to subsidize growth of the archive by 10Tb/year.  In 2017, Doug Boyer, lead PI was awarded a <a href="https://www.idigbio.org/wiki/index.php/MorphoSource" target="_blank">3-year NSF Advancing Biological Infrastructure Development grant</a> (<a href="https://nsf.gov/awardsearch/showAward?AWD_ID=1661386&HistoricalAwards=false" target="_blank">DBI-1661386</a>) in collaboration with Penn State Univ. (<a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1661132&HistoricalAwards=false" target="_blank">DBI-1661132</a>). At that time, Duke made further commitments to support staff and storage growth for at least 5 years after the grant period. Duke Libraries committed to staff MorphoSource with one developer in perpetuity.  The project leadership continues to seek external funds and additional partners to increase the sustainability of the archive service.  Currently MorphoSource accepts contributions at no charge. Ultimately, MorphoSource may require users and contributors to sponsor community level data access.</div>
+        
+        <h3>Institutional Governance</h3>
+        <div>MorphoSource is being integrated into the Duke Library Digital Repository as a Samvera/Fedora platform. Thus it benefits from governance at a high level in Duke University’s IT infrastructure.</div>
+
+        <h3>Staff & Human Resources</h3>
+        <div>The Duke Library Digital Repository <a href="https://library.duke.edu/about/directory/dept/497" target="_blank">Software Development and Integration Service</a> includes a staff of five full time developers, a metadata analyst, and DevOps analyst, who are available for helping with MorphoSource issues. In addition, MorphoSource has three full time personnel including a Product Manager and Lead Developer (J. Winchester) and two additional developers (S. Choy and J. Triplett).</div>
+
+        <h3>Current development priorities</h3>
+        <div>The MorphoSource team currently has two sets of priorities mandated by current funding. 1) Refactor the site as Digital Preservation Stack (using Samvera/Fedora). 2) Support large scale institutional users representing collaborators in the <a href="https://www.idigbio.org/wiki/index.php/OVert:_Open_Exploration_of_Vertebrate_Diversity_in_3D#Project_Summary" target="_blank">oVert TCN</a> project. Our hope is to transition fully to the refactored site by the end of 2020. In the meantime, we will support modifications to the current platform as well.</div>
+
+        <h3>MorphoSource in 4 years</h3>
+        <div>The development work underway will help us do several things that users will notice: 1) improve the ability of museum collections to manage data representing their specimens by changing how access/edit rights are controlled, 2) improve the diversity/complexity of 3D data that can be preserved, 3) streamline the user interface to help with discovery and contribution of data. 4) Richer 3D previews including color previews and volume previews (of CT scans). On the backend, MorphoSource will be a built on a widely used and endorsed open access preservation platform Samvera/Fedora.  This will make MorphoSource’s data even more secure, allow it to be sustained because it is written in a broadly implemented preservation platform, and provide potential for containerization as well interoperability with other databases. In terms of data, we expect to increase our taxonomic representation by 400-500%, and to add non-biological datasets including archaeological datasets, and cultural heritage objects.  These latter two areas will require new search/discovery interfaces. We expect our user base to approach 20,000.</div>
+
+        <h3>Repository scale</h3>
+
+        <b>Users:</b> 6,164<br/>
+        <b>Contributors:</b> 712<br/>
+        <b>Datasets:</b> 44,677<br/>
+        <b>Specimens:</b> 4,803<br/>
+        <b>Species:</b> 11,868<br/>
+        <b>Storage footprint:</b> 18TB<br/>
+        <b>Downloads:</b> 91,890<br/>
+        <b>Views:</b> 1,218,175<br/>
+        
+    </div>
+</div><!-- end textContent -->
