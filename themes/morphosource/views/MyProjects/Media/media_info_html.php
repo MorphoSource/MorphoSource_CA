305a6e6237f7d5d964d3938fe938278257f00826
Simon Choy <simon.choy@duke.edu>
2018-07-19 17:23:35 -0400

    check specimen existence before displaying specimen name in derived from media

--- themes/morphosource/views/MyProjects/Media/media_info_html.php
+++ themes/morphosource/views/MyProjects/Media/media_info_html.php
@@ -237,7 +237,11 @@
 					case "derived_from_media_id":
 						if($t_media->get("derived_from_media_id")){
 							$t_parent = new ms_media($t_media->get("derived_from_media_id"));
-							print "<b>M".$t_parent->get("media_id")."</b>, ".$t_specimen->getSpecimenName($t_parent->get("specimen_id"));
+                            if ($t_specimen == null) {
+                                print "<b>M".$t_parent->get("media_id")."</b>";                                
+                            } else {
+                                print "<b>M".$t_parent->get("media_id")."</b>, ".$t_specimen->getSpecimenName($t_parent->get("specimen_id"));
+                            }
 						}
 					break;
 					# ------------------------------
