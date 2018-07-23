cf33b31427f8758f3c7ef6717d5c9ff16f5eb8d4
Julie Winchester <julia.m.winchester@gmail.com>
2018-07-23 14:38:37 -0400

    Added project browse filter and revamped look

--- themes/morphosource/views/Browse/project_list_html.php
+++ themes/morphosource/views/Browse/project_list_html.php
@@ -32,15 +32,27 @@
 	<div id="browseList">
 		<div class="tealRule"><!-- empty --></div>
 		<H2><?php print _t("Projects"); ?></H2>
+		<div class='' style='padding-left: 10px;'>
+			Filter: <input type='text' name='filter' value='' onkeyup='filterProjectBrowse(this.value); return false;' size='20' style='border:1px solid #828282; margin-right: 10px; margin-left: 5px;'/>
+			<span id='numberProjectsFiltered'></span>
+			<br/><br/>
+		</div><!-- end class list-filter -->
 <?php
 	if($q_project->numRows() > 0){
-		print "<div class='browseListScrollContainer'>";
+		print "<div class='browseListScrollContainer' style='padding-right: 10px;'>";
 		while($q_project->nextRow()){
 			$t_project->load($q_project->get("project_id"));
-			print "<div class='browseItem'><a href='#' onClick='highlightLink(this); jQuery(\"#specimenResults\").load(\"".caNavUrl($this->request, '', 'Browse', 'specimenResults', array('project_id' => $q_project->get("project_id")))."\"); return false;' class='blueText".(($q_project->get("project_id") == $pn_browse_project_id) ? " browseItemSelected" : "")."'>".$q_project->get("name")."</a>";
+			
+			// print "<a href='#' onClick='highlightLink(this); jQuery(\"#specimenResults\").load(\"".caNavUrl($this->request, '', 'Browse', 'specimenResults', array('project_id' => $q_project->get("project_id")))."\"); return false;' class='blueText".(($q_project->get("project_id") == $pn_browse_project_id) ? " browseItemSelected" : "")."'>".$q_project->get("name")."</a>";
+			// print caNavLink($this->request, _t($q_project->get("name")), 'blueText', 'Detail', 'ProjectDetail', 'Show', array('project_id' => $q_project->get("project_id")));
+			
 			if($t_project->get("publication_status")){
-				print "<div style='margin:2px 0px 0px 20px;'>";
+				print "<div class='browseItem' style='margin-top: 25px; padding-top: 20px; border-top: 1px solid #578686;'>";
+
+				print "<h2 style='padding: 0px'>".$q_project->get("name")."</h2>";
+
 				if($q_project->get("abstract")){
+					print "<div style='margin: 15px 0px 0px 0px;'>";
 					if(mb_strlen($q_project->get("abstract")) > 250){
 						print mb_substr($q_project->get("abstract"), 0, 250);
 						print "... <span style='text-decoration:underline;' id='abstract".$q_project->get("project_id")."'>More &rsaquo;</span>";
@@ -50,8 +62,10 @@
 					}else{
 						print $q_project->get("abstract");	
 					}
-					print "<br/>";
+					print "</div>";
 				}
+
+				print "<div style='margin-top: 10px;'>";
 				$va_members = array();
 				$va_members = $t_project->getMembers();
 				if(sizeof($va_members) > 0){
@@ -70,10 +84,16 @@
 				print "<b>Data:</b> ";
 				print (int)$t_project->numAllMedia(null, true)." published media";
 				print ", ".(int)$t_project->numSpecimens()." specimens";
-				print "<br/>".caNavLink($this->request, _t("Project Info"), 'button buttonSmall', 'Detail', 'ProjectDetail', 'Show', array('project_id' => $q_project->get("project_id")));
 				print "</div>";
+
+				print "<div style='margin-top: 15px;'>";
+				print caNavLink($this->request, _t("Project Page"), 'button buttonSmall', 'Detail', 'ProjectDetail', 'Show', array('project_id' => $q_project->get("project_id")));
+				print "<a href='#' style='margin-left: 15px;' class='button buttonSmall' onClick='highlightLink(this); jQuery(\"#specimenResults\").load(\"".caNavUrl($this->request, '', 'Browse', 'specimenResults', array('project_id' => $q_project->get("project_id")))."\"); return false;'>List project specimens</a>";
+				print "</div>";
+				print "</div>";
+
 			}
-			print "</div>";		
+					
 		}
 		print "</div>";
 	}else{
@@ -95,4 +115,16 @@
 <?php	
 	}
 ?>
+	filterProjectBrowse = function (searchText) {
+		if (!searchText) {
+			jQuery('#browseList').find('.browseItem').show();
+			jQuery('#numberProjectsFiltered').text('');
+			return;
+		}
+		jQuery('#browseList').find('.browseItem').hide();
+		var filtered = jQuery('#browseList')
+			.find('.browseItem:iContains('+searchText+')');
+		filtered.show();
+		jQuery('#numberProjectsFiltered').text(filtered.length + ' projects found');
+	}
 </script>
