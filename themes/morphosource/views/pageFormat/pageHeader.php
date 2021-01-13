4bb2a9396b7c19b13171018b91f6b5867dea40d0
Julia Winchester <julia.m.winchester@gmail.com>
2021-01-13 10:23:48 -0500

    Disable logging in, user registration, auto-logout all users, and add site down notification
    Conflicts:
    	themes/morphosource/views/pageFormat/pageHeader.php

--- themes/morphosource/views/pageFormat/pageHeader.php
+++ themes/morphosource/views/pageFormat/pageHeader.php
@@ -1,3 +1,4 @@
+<?php $this->request->deauthenticate(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -203,7 +204,8 @@
 		<div style="clear:both; height:1px;"><!-- empty --></div>
 		<div style="margin-top: 20px; margin-bottom: 20px; border: 1px solid #003880; border-radius: 5px; width: 860px;">
 			<div style="margin:15px">
-				<span><b>January 4th Update:</b> MorphoSource 2.0 is coming the week of January 19th! We are provisionally planning to bring the current site down for maintenance on Tuesday, January 12th. Our target date for launching MorphoSource 2.0 is Tuesday, January 19th, with the possibility of the repository coming online one or two days later that week. MorphoSource 2.0 includes many new features like web viewing of CT data, expanded support for 3D modalities like photogrammetry, and new support for cultural heritage objects.</span>
+				<h1 style="text-align: center; color: red;">MORPHOSOURCE IS TEMPORARILY UNAVAILABLE</h1>
+				<div style="margin-top: 5px;">The MorphoSource 3D Data Repository is currently unavailable as we prepare for the launch of the new and improved MorphoSource 2.0 platform. You can still browse and preview media, specimens, and projects on the current site, but for now you will not be able to log in, download, or manage data. The new version of the repository will come with a refreshed user interface and numerous new features including web viewing of CT data, expanded support for 3D modalities like photogrammetry, and support for cultural heritage objects. We expect to launch MorphoSource 2.0 the week of Tuesday, January 19th, but we will keep this space updated with the latest information about the 2.0 launch.</div>
 			</div>
 		</div>
 	</div><!-- end header -->
